6c6d7aa45053c5cd6e66cd93d1aad7056333af20
Sherman <syan1689@gmail.com>
2024-10-27 23:13:54 -0700

    add firebase package

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,4 +40,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation(platform("com.google.firebase:firebase-bom:33.5.1"))
+    implementation("com.google.firebase:firebase-analytics")
 }
